298e1a2271a73ddb954bfa3fb06f10830c2c93ce
Vickram Ravichandran <vickramganesh@outlook.com>
2018-02-17 12:36:16 -0500

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-//var jslint = require('gulp-jslint');
+var eslint = require('gulp-eslint');
 
 var DEST = '.';
 
 gulp.task('scripts', function () {
     return gulp.src('angular-auto-complete.js')
-  //      .pipe(jslint())
+        .pipe(eslint())
+        .pipe(eslint.format())
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest(DEST));
